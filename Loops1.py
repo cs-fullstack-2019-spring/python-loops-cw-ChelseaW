89a8cc128dc80776636227be7def3653e69d3d78
ChelseaW <c.winfrey93@yahoo.com>
2019-01-18 15:48:49 -0600

    LoopsCW

---
+++ Loops1.py
@@ -0,0 +1,38 @@
+def main():
+    Exercise1()
+    # Exercise2()
+
+# Print -20 to and including 50. Use any loop you want.
+
+
+# Exercise1
+def Exercise1():
+    # while < -20:
+    #     print(<=50)
+    for eachEl in range(-20,50)
+    if >-20:
+        print('Not It')
+
+    else:
+            print(eachEl)
+
+
+
+ # Exercise2
+
+    # Create a loop that prints even numbers from 0 to and including 20.
+
+def Exercise2():
+
+
+    # nums = [15,16,17,18,19,20]
+    for eachEl in range(0,21,2):
+        print (eachEl)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
